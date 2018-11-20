f086dea140d001fad42e523473b4decedfcb4d66
huststl <huststl@foxmail.com>
2018-11-20 09:25:53 +0800

    leetcod011题

---
+++ src/com/huststl/year18/nov/day20/Main020.java
@@ -0,0 +1,35 @@
+package com.huststl.year18.nov.day20;
+
+//盛最多的水 难度（medium)
+public class Main020 {
+
+
+    //解法1
+    public int maxArea01(int[] height){
+        int res = 0,i=0,j=height.length-1;
+
+        while (i<j){
+            res = Math.max(res,Math.min(height[i],height[j]));
+
+            if(height[i] < height[j])
+                ++i;
+            else
+                --j;
+        }
+        return res;
+    }
+
+    //解法2
+    public int maxArea02(int[] height){
+        int res = 0,i=0,j=height.length-1;
+        while (i<j){
+            int h = Math.min(height[i],height[j]);
+            res = Math.max(res,h*(j-i));
+            while (i<j && h==height[i])
+                ++i;
+            while (i<j && h==height[j])
+                --j;
+        }
+        return res;
+    }
+}
