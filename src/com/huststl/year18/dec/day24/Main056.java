f23fc08566817da4031f96693de3635413572cc0
huststl <huststl@foxmail.com>
2018-12-24 10:18:51 +0800

    leetcod056题

---
+++ src/com/huststl/year18/dec/day24/Main056.java
@@ -0,0 +1,63 @@
+package com.huststl.year18.dec.day24;
+
+
+import datastruc.Interval;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 8:01 PM 2018/07/12.
+ */
+public class Main056 {
+
+    //解法1
+    public List<Interval> merge(List<Interval> intervals) {
+        // sort start&end
+        int n = intervals.size();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        // loop through
+        List<Interval> res = new ArrayList<>();
+        // j is start of interval.
+        for (int i = 0, j = 0; i < n; i++) {
+            if (i == n - 1 || starts[i + 1] > ends[i]) {
+                res.add(new Interval(starts[j], ends[i]));
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+
+    //解法2
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> list = new ArrayList<Interval>();
+        Comparator<Interval> comparator = new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                if (o1.start == o2.start)
+                    return o1.end - o2.end;
+                return o1.start - o2.start;
+            }
+        };
+        Collections.sort(intervals, comparator);
+        for (Interval interval : intervals)
+            if (list.size() == 0 || list.get(list.size() - 1).end < interval.start)
+                list.add(new Interval(interval.start, interval.end));
+            else
+                list.get(list.size() - 1).end = Math.max(interval.end, list.get(list.size() - 1).end);
+        return list;
+    }
+
+
+
+}
